62ecd1fad27135a743801268ec2fdbc0c73e5360
Morteza Eslami <morteza.ess@gmail.com>
2019-10-30 22:57:35 +0330

    rm api

--- api/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = async (req, res) => {
-  const query = req.query //{ query } = req
-  res.end(`Hello ${query.name}, you just parsed the request body!`)
-}
--- testnow/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (req, res) => {
-  //const body = req.body //{ body } = req
-  //res.end(`Hello ${body.name}, you just parsed the request body!`)
-  res.end(`Hello`)
-
-}
