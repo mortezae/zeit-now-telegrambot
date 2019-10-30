a581e81455b0c5e34804abde57310cd7ea726239
Morteza Eslami <morteza.ess@gmail.com>
2019-10-30 22:35:51 +0330

    First actual commit for bot

---
+++ index.js
@@ -0,0 +1,23 @@
+const TelegramBot = require('node-telegram-bot-api')
+
+var token = '861772445:AAE-yaLFzjquQQJiWfZCBHRRf1_sFuR643M'
+var bot   = new TelegramBot(token)
+
+module.exports =  async function (req, res) {
+  var msg    = req.body.message
+  var cbq    = req.body.callback_query
+
+  if (msg) {
+    var chatId = msg.chat.id
+
+    if (msg.text != null) {
+      if (msg.text.match(/\/start/)) {
+        bot.sendMessage(chatId, 'Welcome!')
+      } else if (msg.text.includes('hi')) {
+        bot.sendMessage(chatId, 'Hello! :)')
+      } else if (msg.text.includes('bye')) {
+        bot.sendMessage(chatId, 'Bye dear!')
+      }
+    } // if message.text
+  }
+}
