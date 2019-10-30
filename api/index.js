91f022c141a2bfc1e86ce20b5021316a358a225d
Morteza Eslami <morteza.ess@gmail.com>
2019-10-30 21:30:29 +0330

    3rd commit

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 module.exports = async (req, res) => {
-  const body = req.body //{ body } = req
-  res.end(`Hello ${body.name}, you just parsed the request body!`)
+  //const body = req.body //{ body } = req
+  //res.end(`Hello ${body.name}, you just parsed the request body!`)
+  res.end(`Hello`)
 }
